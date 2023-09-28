b4bcf7378e0031e2be234e18e901b21404c4274e
leticiamarts <leticiamarts99@gmail.com>
2023-09-28 18:22:56 -0300

    refactor: Update code (EN)

--- main.py
+++ main.py
@@ -1,107 +1,111 @@
-# Modelagem de política de crédito simplificada usando lógica fuzzy
-# Para o nosso exemplo simplificado o score varia de 0-1000, em que 0 indica um cliente ruim e 1000 um cliente bom.
+# Simplified credit policy modeling using fuzzy logic
+# For our simplified example, the score ranges from 0-1000, where 0 indicates a poor customer and 1000 a good customer.
 from skfuzzy import control as ctrl
 import matplotlib.pyplot as plt
 import skfuzzy as fuzz
 import numpy as np
+import os
 
-# Entradas
-score_mercado = ctrl.Antecedent(np.arange(0, 1001, 1), "score_mercado")
-score_interno = ctrl.Antecedent(np.arange(0, 1001, 1), "score_interno")
-engajamento = ctrl.Antecedent(np.arange(0, 5000, 1), "engajamento")
+# Inputs
+market_score = ctrl.Antecedent(np.arange(0, 1001, 1), "market_score")
+internal_score = ctrl.Antecedent(np.arange(0, 1001, 1), "internal_score")
+engagement = ctrl.Antecedent(np.arange(0, 5000, 1), "engagement")
 
-# Saída
-politica_cliente = ctrl.Consequent(np.arange(0, 1001, 1), "politica_de_risco")
+# Output
+customer_policy = ctrl.Consequent(np.arange(0, 1001, 1), "credit_policy")
 
-# Fuzzificação
-score_mercado["RATING 1"] = fuzz.trimf(score_mercado.universe, [890, 900, 1000])
-score_mercado["RATING 2"] = fuzz.trimf(score_mercado.universe, [750, 800, 890])
-score_mercado["RATING 3"] = fuzz.trimf(score_mercado.universe, [570, 700, 750])
-score_mercado["RATING 4"] = fuzz.trimf(score_mercado.universe, [390, 500, 570])
-score_mercado["RATING 5"] = fuzz.trimf(score_mercado.universe, [240, 300, 390])
-score_mercado["RATING 6"] = fuzz.trimf(score_mercado.universe, [0, 200, 240])
-score_interno["RATING 1"] = fuzz.trimf(score_interno.universe, [890, 900, 1000])
-score_interno["RATING 2"] = fuzz.trimf(score_interno.universe, [750, 800, 890])
-score_interno["RATING 3"] = fuzz.trimf(score_interno.universe, [570, 700, 750])
-score_interno["RATING 4"] = fuzz.trimf(score_interno.universe, [390, 500, 570])
-score_interno["RATING 5"] = fuzz.trimf(score_interno.universe, [240, 300, 390])
-score_interno["RATING 6"] = fuzz.trimf(score_interno.universe, [0, 200, 240])
-engajamento["alto"] = fuzz.trimf(engajamento.universe, [300, 1000, 5000])
-engajamento["medio"] = fuzz.trimf(engajamento.universe, [100, 200, 300])
-engajamento["baixo"] = fuzz.trimf(engajamento.universe, [0, 50, 100])
+# Fuzzification
+market_score["RATING 1"] = fuzz.trimf(market_score.universe, [890, 900, 1000])
+market_score["RATING 2"] = fuzz.trimf(market_score.universe, [750, 800, 890])
+market_score["RATING 3"] = fuzz.trimf(market_score.universe, [570, 700, 750])
+market_score["RATING 4"] = fuzz.trimf(market_score.universe, [390, 500, 570])
+market_score["RATING 5"] = fuzz.trimf(market_score.universe, [240, 300, 390])
+market_score["RATING 6"] = fuzz.trimf(market_score.universe, [0, 200, 240])
+internal_score["RATING 1"] = fuzz.trimf(internal_score.universe, [890, 900, 1000])
+internal_score["RATING 2"] = fuzz.trimf(internal_score.universe, [750, 800, 890])
+internal_score["RATING 3"] = fuzz.trimf(internal_score.universe, [570, 700, 750])
+internal_score["RATING 4"] = fuzz.trimf(internal_score.universe, [390, 500, 570])
+internal_score["RATING 5"] = fuzz.trimf(internal_score.universe, [240, 300, 390])
+internal_score["RATING 6"] = fuzz.trimf(internal_score.universe, [0, 200, 240])
+engagement["high"] = fuzz.trimf(engagement.universe, [300, 1000, 5000])
+engagement["medium"] = fuzz.trimf(engagement.universe, [100, 200, 300])
+engagement["low"] = fuzz.trimf(engagement.universe, [0, 50, 100])
 
-politica_cliente["grau de risco 1"] = fuzz.trimf(
-    politica_cliente.universe, [850, 950, 1000]
+customer_policy["risk degree 1"] = fuzz.trimf(
+    customer_policy.universe, [850, 950, 1000]
 )
-politica_cliente["grau de risco 2"] = fuzz.trimf(
-    politica_cliente.universe, [750, 850, 900]
-)
-politica_cliente["grau de risco 3"] = fuzz.trimf(
-    politica_cliente.universe, [650, 750, 800]
-)
-politica_cliente["grau de risco 4"] = fuzz.trimf(
-    politica_cliente.universe, [250, 500, 700]
-)
-politica_cliente["grau de risco 5"] = fuzz.trimf(
-    politica_cliente.universe, [0, 250, 300]
-)
+customer_policy["risk degree 2"] = fuzz.trimf(customer_policy.universe, [750, 850, 900])
+customer_policy["risk degree 3"] = fuzz.trimf(customer_policy.universe, [650, 750, 800])
+customer_policy["risk degree 4"] = fuzz.trimf(customer_policy.universe, [250, 500, 700])
+customer_policy["risk degree 5"] = fuzz.trimf(customer_policy.universe, [0, 250, 300])
 
-# Inferência
-regra1 = ctrl.Rule(
-    engajamento["baixo"] & (score_mercado["RATING 6"] | score_interno["RATING 6"])
-    | (score_mercado["RATING 5"] | score_interno["RATING 5"])
-    | (score_mercado["RATING 4"] | score_interno["RATING 4"])
-    | (score_mercado["RATING 3"] | score_interno["RATING 3"]),
-    politica_cliente["grau de risco 5"],
+# Inference
+rule1 = ctrl.Rule(
+    engagement["low"] & (market_score["RATING 6"] | internal_score["RATING 6"])
+    | (market_score["RATING 5"] | internal_score["RATING 5"])
+    | (market_score["RATING 4"] | internal_score["RATING 4"])
+    | (market_score["RATING 3"] | internal_score["RATING 3"]),
+    customer_policy["risk degree 5"],
 )
-regra2 = ctrl.Rule(
-    engajamento["baixo"] & (score_mercado["RATING 1"] | score_interno["RATING 1"])
-    | (score_mercado["RATING 2"] | score_interno["RATING 2"]),
-    politica_cliente["grau de risco 4"],
+rule2 = ctrl.Rule(
+    engagement["low"] & (market_score["RATING 1"] | internal_score["RATING 1"])
+    | (market_score["RATING 2"] | internal_score["RATING 2"]),
+    customer_policy["risk degree 4"],
 )
-regra3 = ctrl.Rule(
-    engajamento["medio"] & (score_mercado["RATING 6"] | score_interno["RATING 6"])
-    | (score_mercado["RATING 5"] | score_interno["RATING 5"])
-    | (score_mercado["RATING 4"] | score_interno["RATING 4"]),
-    politica_cliente["grau de risco 5"],
+rule3 = ctrl.Rule(
+    engagement["medium"] & (market_score["RATING 6"] | internal_score["RATING 6"])
+    | (market_score["RATING 5"] | internal_score["RATING 5"])
+    | (market_score["RATING 4"] | internal_score["RATING 4"]),
+    customer_policy["risk degree 5"],
 )
-regra4 = ctrl.Rule(
-    engajamento["medio"] & (score_mercado["RATING 1"] | score_interno["RATING 1"])
-    | (score_mercado["RATING 2"] | score_interno["RATING 2"])
-    | (score_mercado["RATING 3"] | score_interno["RATING 3"]),
-    politica_cliente["grau de risco 4"],
+rule4 = ctrl.Rule(
+    engagement["medium"] & (market_score["RATING 1"] | internal_score["RATING 1"])
+    | (market_score["RATING 2"] | internal_score["RATING 2"])
+    | (market_score["RATING 3"] | internal_score["RATING 3"]),
+    customer_policy["risk degree 4"],
 )
-regra5 = ctrl.Rule(
-    engajamento["alto"] & (score_mercado["RATING 6"] | score_interno["RATING 6"]),
-    politica_cliente["grau de risco 4"],
+rule5 = ctrl.Rule(
+    engagement["high"] & (market_score["RATING 6"] | internal_score["RATING 6"]),
+    customer_policy["risk degree 4"],
 )
-regra6 = ctrl.Rule(
-    engajamento["alto"] & (score_mercado["RATING 5"] | score_interno["RATING 5"])
-    | (score_mercado["RATING 4"] | score_interno["RATING 4"]),
-    politica_cliente["grau de risco 3"],
+rule6 = ctrl.Rule(
+    engagement["high"] & (market_score["RATING 5"] | internal_score["RATING 5"])
+    | (market_score["RATING 4"] | internal_score["RATING 4"]),
+    customer_policy["risk degree 3"],
 )
-regra7 = ctrl.Rule(
-    engajamento["alto"] & (score_mercado["RATING 3"] | score_interno["RATING 3"]),
-    politica_cliente["grau de risco 2"],
+rule7 = ctrl.Rule(
+    engagement["high"] & (market_score["RATING 3"] | internal_score["RATING 3"]),
+    customer_policy["risk degree 2"],
 )
-regra8 = ctrl.Rule(
-    engajamento["alto"] & (score_mercado["RATING 1"] | score_interno["RATING 1"])
-    | (score_mercado["RATING 2"] | score_interno["RATING 2"]),
-    politica_cliente["grau de risco 1"],
+rule8 = ctrl.Rule(
+    engagement["high"] & (market_score["RATING 1"] | internal_score["RATING 1"])
+    | (market_score["RATING 2"] | internal_score["RATING 2"]),
+    customer_policy["risk degree 1"],
 )
-politica_ctrl = ctrl.ControlSystem(
-    [regra1, regra2, regra3, regra4, regra5, regra6, regra7, regra8]
+policy_ctrl = ctrl.ControlSystem(
+    [rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8]
 )
-politica_simulador = ctrl.ControlSystemSimulation(politica_ctrl)
+policy_simulator = ctrl.ControlSystemSimulation(policy_ctrl)
 
-# Simulação
-politica_simulador.input["score_mercado"] = 200
-politica_simulador.input["score_interno"] = 230
-politica_simulador.input["engajamento"] = 90
+# Simulation
+policy_simulator.input["market_score"] = 200
+policy_simulator.input["internal_score"] = 230
+policy_simulator.input["engagement"] = 90
 
-# Deffuzificação
-politica_simulador.compute()
-print(politica_simulador.output["politica_de_risco"])
-output_value = politica_simulador.output["politica_de_risco"]
-politica_cliente.view(sim=politica_simulador)
-plt.savefig("politica_de_risco_plot.png")
+# Defuzzification
+policy_simulator.compute()
+print(policy_simulator.output["credit_policy"])
+output_value = policy_simulator.output["credit_policy"]
+customer_policy.view(sim=policy_simulator)
+
+base_filename = "credit_policy_plot"
+output_folder = "output"
+counter = 1
+
+while True:
+    filename = os.path.join(output_folder, f"{base_filename}{counter}.png")
+    if not os.path.exists(filename):
+        break
+    counter += 1
+
+plt.savefig(filename)
